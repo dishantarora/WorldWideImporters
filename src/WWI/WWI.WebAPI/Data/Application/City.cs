f95f298d3f1ba5abf84f85277f7e294604105061
Dishant Arora <dishantarora@outlook.com>
2022-11-20 17:51:49 +0530

    Created City Data Model.  - Added few parameters.  - Configured City data model with People data model.

---
+++ src/WWI/WWI.WebAPI/Data/Application/City.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace WWI.WebAPI.Data.Application
+{
+    public class City
+    {
+        public int CityID { get; set; }
+        public string CityName { get; set; }
+        public int StateProvinceId { get; set; }
+        public string Location { get; set; }
+        public long LatestRecordedPopulation { get; set; }
+        public DateTime ValidFrom { get; set; }
+        public DateTime ValidTo { get; set; }
+
+        // Configuring one-to-many relationship with People.
+        public int LastEditedBy { get; set; }
+        public People People { get; set; }
+    }
+}
